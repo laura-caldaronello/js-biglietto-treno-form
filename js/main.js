bda96b4977c69b33639aaac8e87e66b68cc2401a
Laura Caldaronello <caldaronello.l@gmail.com>
2021-02-22 15:04:43 +0100

    Sembra funzionante

--- js/main.js
+++ js/main.js
@@ -12,15 +12,21 @@ function() {
   if (outputAge == "range1") {
     var offer = 0.8 * whole;
     var offer = offer.toFixed(2);
+    document.getElementById("outputOffer").innerHTML = "Sconto Minorenne";
   }
   else if (outputAge == "range2") {
     var offer = whole.toFixed(2);
+    document.getElementById("outputOffer").innerHTML = "Prezzo intero";
   }
   else if (outputAge == "range3") {
     var offer = 0.6 * whole;
-    var offer = offer.toFixed(2);  
+    var offer = offer.toFixed(2);
+    document.getElementById("outputOffer").innerHTML = "Sconto Over 65";
   }
-  document.getElementById("outputOffer").innerHTML = offer + " €";
+  document.getElementById("outputPrice").innerHTML = offer + " €";
 
+  document.getElementById("outputCarriage").innerHTML = Math.floor(Math.random() * 10) + 1;
+
+  document.getElementById("outputCP").innerHTML = Math.floor(Math.random() * 10000) + 90000;
 }
 );
